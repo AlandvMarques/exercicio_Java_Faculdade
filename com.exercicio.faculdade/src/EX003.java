ffaf14168fb7f4d3e45426577d7cbe7cc21047d2
AlandvMarques <alandvmarques@gmail.com>
2023-08-30 20:54:37 -0300

    resolução da questão 3

---
+++ com.exercicio.faculdade/src/EX003.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EX003 {
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de maçãs: ");
+        int quantMacas = input.nextInt();
+
+        double valorTotal;
+
+        if (quantMacas > 12) {
+            valorTotal = quantMacas * 1.00;
+        } else {
+            valorTotal = quantMacas * 1.30;
+        }
+
+        System.out.println("O valor total da compra foi de R$ " + valorTotal);
+
+        input.close();
+    } 
+}
