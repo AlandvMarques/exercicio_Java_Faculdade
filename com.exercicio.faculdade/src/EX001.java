6e679f74eeb91eaaad5d1e29cb64b3ab55119e4d
AlandvMarques <alandvmarques@gmail.com>
2023-08-30 20:17:59 -0300

    exercicio numero 1 da lista

---
+++ com.exercicio.faculdade/src/EX001.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EX001 {
+    
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Qual sua idade?");
+
+        int idade = input.nextInt();
+
+        if(idade > 10){
+            System.out.println("MAIOR QUE 10!");
+        }else{
+            System.out.println("MAIOR QUE 10!");
+        }
+        
+
+        
+
+        
+    
+
+
+    }
+}
