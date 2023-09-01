f0cffbf39c8171deae1bb96bfec6490b31341dbb
AlandvMarques <alandvmarques@gmail.com>
2023-08-31 22:22:12 -0300

    resolução da questão 9 da lista de exercícios

---
+++ com.exercicio.faculdade/src/Ex009.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ex009 {
+    public static void main(String[] args) {
+
+        int horasSemanais = 40;
+        int semanasPorMes = 4;
+        double salarioTotal;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o número de horas trabalhadas em um mês: ");
+        int horasTrabalhadas = input.nextInt();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Digite o salário por hora: ");
+        double salarioPorHora = input2.nextDouble();
+
+        if (horasTrabalhadas <= horasSemanais * semanasPorMes) {
+            salarioTotal = horasTrabalhadas * salarioPorHora;
+        } else {
+            int horasExtras = horasTrabalhadas - (horasSemanais * semanasPorMes);
+            double salarioHoraExtra = salarioPorHora * 1.5;
+            salarioTotal = (horasSemanais * semanasPorMes * salarioPorHora) + (horasExtras * salarioHoraExtra);
+        }
+
+        System.out.println("Salário total do funcionário: R$ " + salarioTotal);
+
+        input.close();
+    }
+}
+    
