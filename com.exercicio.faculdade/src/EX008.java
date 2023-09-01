9c9a0e8506840111ab58ca1c7018419e9b9fd595
AlandvMarques <alandvmarques@gmail.com>
2023-08-31 22:04:44 -0300

    resolução da questão 8 da lista de exercícios número 1

---
+++ com.exercicio.faculdade/src/EX008.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EX008 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digita a hora do íbnicio do jogo: ");
+        int horaInicial = input.nextInt();
+        
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Digita a hor do fim do jogo: ");
+        int horaFinal = input2.nextInt();
+
+        int duracao;
+
+        if(horaInicial < horaFinal){
+            duracao = horaFinal - horaInicial; 
+        }else{
+            duracao = 24 - horaInicial + horaFinal;
+        }
+        System.out.println("A duração do jogo foi " + duracao + " horas!");
+    }
+}
